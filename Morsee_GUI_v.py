0bc5d8bf0ae1e554c33aff5175c7d91f610fc64a
Piankov <piankovpe@gmail.com>
2022-11-07 23:35:54 +0600

    Add copy/cut/paste for r.mouse click menu. Add button to encode.

--- Morsee_GUI_v.py
+++ Morsee_GUI_v.py
@@ -3,8 +3,7 @@ from tkinter import *
 from tkinter import messagebox
 import pyperclip
 
-
-# before OOP implementation
+# before OOP implementation (maybe ignore OOP in this one, we'll see)
 dot = '.'
 line = '_'
 morse_encoding = {
@@ -86,19 +85,98 @@ def decode(morse: str) -> str:
                 list_decoded.append(morse_decoding[letter])
         list_decoded.append(" ")
     return "".join(list_decoded)
-# before OOP implementation
+
+
+def encode_button_command():
+    if text_to_encode.get("1.0", "end-1c"):  # end-1c, deletes 1 character from end, needed to remove empty line.
+        text_encoded.config(state="normal")
+        text_encoded.delete("1.0", END)
+        text = text_to_encode.get("1.0", END)
+        # print(text)
+        encoded_text = encode(text)
+        # print(encoded_text)
+        text_encoded.insert("1.0", encoded_text)
+        text_encoded.config(state="disabled")
+    else:
+        messagebox.showinfo(title="Empty",
+                            message="There's no Text to Encode.\nPlease fill it.",
+                            icon="question",  # removing sound, icons: info, warning - cause warning sound.
+                            )
+
+
+# there's fully working solution in Scraps, but need to try it myself, at least once.
+# right_click showing menu
+def right_click(event):
+    try:
+        menu.tk_popup(event.x_root, event.y_root)
+    finally:
+        menu.grab_release()
+
+
+# copy command #
+def right_click_copy():
+    try:
+        selected_text = main_window.selection_get()
+        main_window.clipboard_clear()
+        main_window.clipboard_append(selected_text)
+    except TclError:
+        pass
+
+
+# cut command #
+def right_click_cut():
+    try:
+        selected_text = main_window.selection_get()
+        main_window.clipboard_clear()
+        main_window.clipboard_append(selected_text)
+        text_to_encode.delete("sel.first", "sel.last")
+    except TclError:
+        pass
+
+
+# paste command #
+def right_click_paste():
+    try:
+        if main_window.focus_get() == text_to_encode:
+            copied_text = main_window.selection_get(selection="CLIPBOARD")
+            text_to_encode.insert(tkinter.INSERT, copied_text)  # tkinter.INSERT current position of a cursor
+    except TclError:
+        pass
+
+
+# delete command #
+def right_click_delete():
+    try:
+        text_to_encode.delete("sel.first", "sel.last")
+    except TclError:
+        pass
+# Menu is working, but I can't find any decent info about, making paste and delete actions universal.
+# It's working fine with just One active text_widget, but if I will ever want to add other widget's
+# guess I will need to add focus_check for every text_widget or entry_widget, looks scrappy.
+# Otherwise, need to find other solution or use One from Scrap_storage,
+# which is maybe universal (tested only for 1 field, anyway).
 
 
 # Gui setup
 main_window = Tk()
+
+# right click menu
+menu = Menu(main_window, tearoff=0)
+menu.add_command(label="Cut", command=right_click_cut)
+menu.add_command(label="Copy", command=right_click_copy)
+menu.add_command(label="Paste", command=right_click_paste)
+menu.add_separator()
+menu.add_command(label="Delete", command=right_click_delete)
+main_window.bind("<Button-3>", right_click)
+
 # icon and window setup
 icon = tkinter.PhotoImage(file='morsee_icon.png')
 main_window.iconphoto(False, icon)
 main_window.title("Morsee")
-main_window.config(
-    padx=50,
-    pady=50,
-)
+main_window.config(padx=50,
+                   pady=50,
+                   )
+
 # input/output frames
 encode_label = Label()
 encode_label.config(
@@ -118,9 +196,10 @@ text_to_encode = Text()
 text_to_encode.config(
     width=75,
     height=10,
-    font=("Ariel", 12),
+    font=("Ariel", 14, "bold"),
     bg="light blue",
     border=2,
+    fg="Black",
 )
 text_to_encode.grid(
     column=1,
@@ -146,10 +225,11 @@ text_encoded = Text()
 text_encoded.config(
     width=75,
     height=10,
-    font=("Ariel", 12),
+    font=("Ariel", 14, "bold"),
     bg="light blue",
-    state="disabled",
     border=2,
+    fg="Black",
+    state="disabled",
 )
 text_encoded.grid(
     column=1,
@@ -157,17 +237,24 @@ text_encoded.grid(
 )
 # encode/copy1 buttons
 encode_button = Button()
-
+encode_button.config(
+    width=25,
+    text="Encode",
+    fg="Black",
+    font=("Ariel", 12),
+    command=encode_button_command,
+)
 encode_button.grid(
     row=2,
     column=1,
     sticky="e",
 )
 
-copy1_button = Button()
-copy1_button.grid(
+copy_button = Button()
+copy_button.grid(
     row=4,
     column=1,
     sticky="e",
 )
+
 main_window.mainloop()
